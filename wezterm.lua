2d4f823fbb1550032447dee74ee020d254d26fef
Alwin Gilang Permana <al.gilang.p@gmail.com>
2025-01-29 07:49:08 +0700

    Create wezterm.lua

---
+++ wezterm.lua
@@ -0,0 +1,26 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("RobotoMono Nerd Font")
+config.default_prog = { "pwsh.exe", "-NoLogo" }
+config.color_scheme = "OneDark (base16)"
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.9
+config.enable_wayland = false
+
+wezterm.on("gui-startup", function(cmd)
+	local screen = wezterm.gui.screens().active
+	local ratio = 0.7 -- Adjust this ratio to set the size of the window
+	local width, height = screen.width * ratio, screen.height * ratio
+	local tab, pane, window = wezterm.mux.spawn_window({
+		position = {
+			x = (screen.width - width) / 2,
+			y = (screen.height - height) / 2,
+			origin = "ActiveScreen",
+		},
+	})
+	window:gui_window():set_inner_size(width, height)
+end)
+
+return config
